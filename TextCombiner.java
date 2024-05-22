9721125ffd07819405aee70d80439ccb19dfecbb
teruuj1 <154354862+teruuj1@users.noreply.github.com>
2024-05-22 17:59:33 +0300

    Add files via upload

--- TextCombiner.java
+++ TextCombiner.java
@@ -6,11 +6,12 @@ public class TextCombiner {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName))) {
             for (File file : files) {
                 try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        writer.write(line);
-                        writer.newLine();
-                    }
+                    int k = Integer.parseInt(reader.readLine());
+                    char[] data = new char[k];
+                    reader.read(data);
+                    int d = Integer.parseInt(reader.readLine());
+                    int p = Integer.parseInt(reader.readLine());
+                    writer.write(data);
                 }
             }
         } catch (IOException e) {
