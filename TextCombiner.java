8dc97dcb2703d1b8765c363d9e72f3b5688e3406
teruuj1 <154354862+teruuj1@users.noreply.github.com>
2024-05-22 17:48:35 +0300

    Add files via upload

--- TextCombiner.java
+++ TextCombiner.java
@@ -1,16 +1,16 @@
 package kontrwork2;
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 
 public class TextCombiner {
-    public static void combineTextFiles(File[] files, String combinedFileName) {
-        try (FileWriter writer = new FileWriter(combinedFileName)) {
+    public static void combineTextFiles(File[] files, String outputFileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName))) {
             for (File file : files) {
-                try (RandomAccessFile raf = new RandomAccessFile(file, "r")) {
-                    raf.readInt(); // skip 'k'
-                    byte[] data = new byte[raf.readInt()];
-                    raf.read(data);
-                    writer.write(new String(data, StandardCharsets.UTF_8));
+                try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        writer.write(line);
+                        writer.newLine();
+                    }
                 }
             }
         } catch (IOException e) {
